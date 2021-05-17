8b1aac7668cbc8424ab415128842423904ff5693
Ahmad-Batah <battah.ahmad@yahoo.com>
2021-05-17 16:14:34 +0300

    Task is done

---
+++ story.js
@@ -0,0 +1,7 @@
+let name = "Ahmad";
+let age = 27;
+let hobby = "swimming"; 
+
+
+
+console.log(`Mr ${name}, in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice!`);
